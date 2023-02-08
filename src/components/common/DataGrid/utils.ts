c306cddb91b9a5f487cd5c48b7f9dc8ceb19c1e5
Parshin D.V <parshin_dv@atommash.ru>
2023-02-08 15:38:11 +0300

    getRelations func

--- src/components/common/DataGrid/utils.ts
+++ src/components/common/DataGrid/utils.ts
@@ -1,5 +1,7 @@
 import * as dot from 'dot-object'
 
+import type { BaseItem, DataGridFilterDef } from './types'
+
 export const filterParams = (params: Record<string, string>): object => {
   const dotsObj = dot.dot(params) as Record<string, string>
 
@@ -10,4 +12,19 @@ export const filterParams = (params: Record<string, string>): object => {
   const preparedParams = dot.object(filteredParams)
 
   return preparedParams
+}
+
+export const getRelations = <T extends BaseItem = BaseItem>(filtersParams: DataGridFilterDef<T>[]): object => {
+  const relations = filtersParams.reduce((acc, item) => {
+    const fieldParts = String(item.name).split('.')
+    if (fieldParts.length === 1) {
+      return acc
+    }
+
+    fieldParts.pop()
+    const relation = fieldParts.join('.')
+
+    return { ...acc, [relation]: true }
+  }, {})
+  return dot.object(relations)
 }
--- src/pages/tracks/tracksIndexPage/TracksIndexPage.tsx
+++ src/pages/tracks/tracksIndexPage/TracksIndexPage.tsx
@@ -1,21 +1,13 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Add } from '@mui/icons-material'
 import { Button } from '@mui/material'
 import { createColumnHelper } from '@tanstack/react-table'
-import * as dot from 'dot-object'
 import type { FC } from 'react'
 import { useCallback } from 'react'
 import { Link } from 'react-router-dom'
 
 import { DataGrid } from '../../../components/common/DataGrid'
 import type { DataGridFilterDef } from '../../../components/common/DataGrid/types'
+import { getRelations } from '../../../components/common/DataGrid/utils'
 import { PageHeader, PageTitle } from '../../../components/ui'
 import { tracksBrowseRoutes } from '../../../core/config'
 import { useTrackSearch } from '../../../core/hooks/tracks'
@@ -45,38 +37,19 @@ const columns = [
   }),
   columnHelper.accessor('artists.id', {
     cell: ({ row }) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
       return row.original.artists.map((artist: ArtistModel) => (
         <div key={`${row.original.id}${artist.id}`}>{artist.title}</div>
       ))
     },
     header: () => 'Исполнители',
   }),
-  /* columnHelper.accessor('artists.id', {
-    cell: ({ row }) => {
-      return row.original.artists.map((artist: ArtistModel) => (
-        <div key={`${row.original.id}${artist.id}`}>{artist.title}</div>
-      ))
-    },
-    header: () => 'Исполнители',
-    minSize: 300,
-    size: 100,
-  }), */
 ]
 
 const filters: DataGridFilterDef<TrackModel>[] = [
   { name: 'id', placeholder: 'id', type: 'text' },
   { name: 'title', placeholder: 'Название', type: 'text' },
   { name: 'credits', placeholder: 'Исполнитель', type: 'text' },
-  /* {
-    name: 'artists.id',
-    options: [
-      { label: 'Все исполнители', value: '' },
-      { label: 'Исполнитель 1', value: '1' },
-      { label: 'Исполнитель 2', value: '2' },
-    ],
-    placeholder: 'Исполнитель',
-    type: 'select',
-  }, */
   {
     asyncSelectOptions: {
       loadOptions: async (term) => {
@@ -88,28 +61,7 @@ const filters: DataGridFilterDef<TrackModel>[] = [
     placeholder: 'Исполнитель',
     type: 'asyncSelect',
   },
-  /* {
-    name: 'release.genres.id',
-    placeholder: 'Жанр',
-    type: 'text',
-  }, */
 ]
-
-// TODO: перенести поулчение связей в хук поиска. Связи можно получить из текущих фильтров
-const getRelations = (filtersParams: DataGridFilterDef<TrackModel>[]): object => {
-  const relations = filtersParams.reduce((acc, item) => {
-    const fieldParts = item.name.split('.')
-    if (fieldParts.length === 1) {
-      return acc
-    }
-
-    fieldParts.pop()
-    const relation = fieldParts.join('.')
-
-    return { ...acc, [relation]: true }
-  }, {})
-  return dot.object(relations)
-}
 
 const TracksIndexPage: FC = () => {
   const handleDelete = useCallback((id: number) => {
